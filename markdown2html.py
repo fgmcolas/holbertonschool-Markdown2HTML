9c4356cc1ef9b36bb524eb80b7ddd6e53253bab6
fgmcolas <https://github.com/fgmcolas>
2024-07-31 11:35:53 +0200

    back to hold save

--- markdown2html.py
+++ markdown2html.py
@@ -9,27 +9,24 @@ import markdown
 def main():
     if len(sys.argv) < 3:
         sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
-        sys.exit(1)
+        exit(1)
 
     md_file = sys.argv[1]
     html_file = sys.argv[2]
 
     if not os.path.exists(md_file):
         sys.stderr.write(f"Missing {md_file}\n")
-        sys.exit(1)
+        exit(1)
 
-    try:
-        with open(md_file, 'r', encoding='utf-8') as md_filename:
-            md_content = md_filename.read()
-            html_content = markdown.markdown(md_content)
 
-        with open(html_file, 'w', encoding='utf-8') as html_filename:
-            html_filename.write(html_content)
-    except Exception as e:
-        sys.stderr.write(f"Error: {e}\n")
-        sys.exit(1)
+    with open(md_file, 'r', encoding='utf-8') as md_filename:
+        md_content = md_filename.read()
+        html_content = markdown.markdown(md_content)
 
-    sys.exit(0)
+    with open(html_file, 'w', encoding='utf-8') as html_filename:
+        html_filename.write(html_content)
+
+    exit(0)
 
 
 if __name__ == "__main__":
