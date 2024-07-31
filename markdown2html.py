488f161a76c14b7c6cf6fdfe6216f811b29b094f
fgmcolas <https://github.com/fgmcolas>
2024-07-31 11:19:20 +0200

    modifying sys.argv len

--- markdown2html.py
+++ markdown2html.py
@@ -6,7 +6,7 @@ import markdown
 
 
 def main():
-    if len(sys.argv) <= 2:
+    if len(sys.argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html",
               file=sys.stderr)
         exit(1)
