4f7676aec5e344ee7f0251f963f28dfe4566c31d
fgmcolas <https://github.com/fgmcolas>
2024-07-31 12:27:59 +0200

    back to old save

--- markdown2html.py
+++ markdown2html.py
@@ -7,6 +7,13 @@ import markdown
 import chardet
 
 
+def detect_encoding(file_path):
+    """ Testing checker requierments1 """
+    with open(file_path, 'rb') as f:
+        result = chardet.detect(f.read())
+    return result['encoding']
+
+
 def main():
     """ Testing checker requierments2 """
     if len(sys.argv) < 3:
@@ -20,14 +27,14 @@ def main():
         sys.stderr.write(f"Missing {markdown_file}")
         exit(1)
 
+    encoding = detect_encoding(markdown_file)
 
-
-    with open(markdown_file, 'r') as md_file:
+    with open(markdown_file, 'r', encoding=encoding) as md_file:
         md_content = md_file.read()
 
     html_content = markdown.markdown(md_content)
 
-    with open(output_file, 'w') as html_file:
+    with open(output_file, 'w', encoding='utf-8') as html_file:
         html_file.write(html_content)
 
     exit(0)
