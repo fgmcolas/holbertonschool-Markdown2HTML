2d1c909b58705f3de8dea8c2e0a511789be33ecf
fgmcolas <https://github.com/fgmcolas>
2024-07-31 11:33:36 +0200

    adding \n and try to handle exceptions

--- markdown2html.py
+++ markdown2html.py
@@ -1,32 +1,34 @@
 #!/usr/bin/python3
-""" Script that convert Markdown to HTML """
+""" Script that converts Markdown to HTML """
 
 import sys
 import os
 import markdown
 
-
 def main():
     if len(sys.argv) < 3:
-        sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
-        exit(1)
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
+        sys.exit(1)
 
     md_file = sys.argv[1]
     html_file = sys.argv[2]
 
     if not os.path.exists(md_file):
-        print(f"Missing {md_file}", file=sys.stderr)
-        exit(1)
+        sys.stderr.write(f"Missing {md_file}\n")
+        sys.exit(1)
 
-    with open(md_file, 'r', encoding='utf-8') as md_filename:
-        md_content = md_filename.read()
-        html_content = markdown.markdown(md_content)
+    try:
+        with open(md_file, 'r', encoding='utf-8') as md_filename:
+            md_content = md_filename.read()
+            html_content = markdown.markdown(md_content)
 
-    with open(html_file, 'w', encoding='utf-8') as html_filename:
-        html_filename.write(html_content)
+        with open(html_file, 'w', encoding='utf-8') as html_filename:
+            html_filename.write(html_content)
+    except Exception as e:
+        sys.stderr.write(f"Error: {e}\n")
+        sys.exit(1)
 
-    exit(0)
-
+    sys.exit(0)
 
 if __name__ == "__main__":
     main()
